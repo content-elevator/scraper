befe657d3016940010b178556e20caa1afe13aee
tiboth <37446840+tiboth@users.noreply.github.com>
2020-05-09 15:05:03 +0300

    added job_id for job_handler

--- lib/scraper/job_handler.ex
+++ lib/scraper/job_handler.ex
@@ -1,20 +1,20 @@
 defmodule JobHandler do
   @moduledoc false
 
-  def handle_scraper_job(url, query, token) do
+  def handle_scraper_job(url, query, token, job_id) do
     # send RECEIVED status for analyzer
-    Scraper.AnalysisUtil.send_status("RECEIVED", token)
+    Scraper.AnalysisUtil.send_status("RECEIVED", token, job_id)
 
     url_content = Scraper.PageScraper.scrape_page(url)
     # send URL_SCRAPING_COMPLETED, status for analyzer
-    Scraper.AnalysisUtil.send_status("URL_SCRAPING_COMPLETED", token)
+    Scraper.AnalysisUtil.send_status("URL_SCRAPING_COMPLETED", token, job_id)
 
     google_search_result = Scraper.GoogleSearchScraper.get_urls_from_search(query)
     # send URL_SCRAPING_COMPLETED, status for analyzer
-    Scraper.AnalysisUtil.send_status("GOOGLE_SCRAPING_COMPLETED", token)
+    Scraper.AnalysisUtil.send_status("GOOGLE_SCRAPING_COMPLETED", token, job_id)
 
     # send result
-    Scraper.AnalysisUtil.send_result(url_content, google_search_result, token)
+    Scraper.AnalysisUtil.send_result(url_content, google_search_result, token, job_id)
   end
 
 end
--- lib/util/analysis_util.ex
+++ lib/util/analysis_util.ex
@@ -1,23 +1,23 @@
 defmodule Scraper.AnalysisUtil do
   @moduledoc false
 
-  def send_status(status, token) do
+  def send_status(status, token, job_id) do
     headers = [{:"Authorization", "Bearer " <> token}, {:"Content-Type", "application/json"}]
     body =
       %{
         status: status
       }
-    HTTPoison.patch(Application.get_env(:scraper, :analysis_server)<>"jobStatus", body, headers)
+    HTTPoison.patch(Application.get_env(:scraper, :analysis_server)<>"/"<>job_id<>"/jobStatus", body, headers)
   end
 
-  def send_result(url_content, google_search_result, token) do
+  def send_result(url_content, google_search_result, token, job_id) do
     headers = [{:"Authorization", "Bearer " <> token}, {:"Content-Type", "application/json"}]
     body =
       %{
         url_content: url_content,
         google_search_result: google_search_result
       }
-    HTTPoison.post(Application.get_env(:scraper, :analysis_server)<>"result", body, headers)
+    HTTPoison.post(Application.get_env(:scraper, :analysis_server)<>"/"<>job_id<>"/result", body, headers)
   end
 
 end
