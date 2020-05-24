8b7cf4ce677f8a74693ad417971b50bb2b96d253
daniftinca <daniftincaalexandru@gmail.com>
2020-05-24 13:58:17 +0300

    config ci

--- config/prod.exs
+++ config/prod.exs
@@ -53,3 +53,10 @@ config :logger, level: :info
 # Finally import the config/prod.secret.exs which loads secrets
 # and configuration from environment variables.
 import_config "prod.secret.exs"
+
+config :"content-scraper-app", ScraperWeb.Endpoint,
+       http: [port: {:system, "PORT"}], # Possibly not needed, but doesn't hurt
+       url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 80],
+       secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
+       server: true
+
