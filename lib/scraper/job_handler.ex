1a26e14c5dcce6c9a10902e2afb3fa9037e2bac9
tiboth <37446840+tiboth@users.noreply.github.com>
2020-05-25 21:46:28 +0300

    added small refactoring

--- lib/scraper/job_handler.ex
+++ lib/scraper/job_handler.ex
@@ -8,12 +8,13 @@ defmodule Scraper.JobHandler do
     Scraper.AnalysisUtil.send_status("RECEIVED", job_id)
     IO.puts "status received sent"
 
+    # send URL_SCRAPING_COMPLETED, status for analyzer
+    Scraper.AnalysisUtil.send_status("URL_SCRAPING_STARTED", job_id)
     url_content = Scraper.PageScraper.scrape_page(url)
     Scraper.AnalysisUtil.send_partial_result(url_content, "title", job_id, false, true)
-    # send URL_SCRAPING_COMPLETED, status for analyzer
-    Scraper.AnalysisUtil.send_status("URL_SCRAPING_COMPLETED", job_id)
+    # send GOOGLE_SCRAPING_STARTED, status for analyzer
+    Scraper.AnalysisUtil.send_status("GOOGLE_SCRAPING_STARTED", job_id)
     IO.puts "status url scraping completed sent"
-
     Scraper.GoogleSearchScraper.get_urls_from_search(query, job_id)
     Scraper.AnalysisUtil.send_partial_result("empty", "empty", job_id, true, false)
 
